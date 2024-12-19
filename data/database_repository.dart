62dddc26607b3bd5256f3d1b59634e7537cb2d65
Max Schwarzkopf <max_schwarzkopf@gmx.de>
2024-12-19 07:37:45 +0100

    feat: add comments

--- data/database_repository.dart
+++ data/database_repository.dart
@@ -1,8 +1,17 @@
 import '../models/meal.dart';
 
+// Unser abstraktes Database Repository
+// Durch das abstract geben wir nur den "Bauplan" vor, welche Methoden andere Klasse implementieren müssen
+// Die eigentliche Logik der einzelnen Methoden geschieht dann in den spezifischen Klassen, nicht hier
 abstract class DatabaseRepository {
+// Fügt ein Meal hinzu
   void addMeal(Meal meal);
+  // gibt uns eine Liste an Meals zurück
   List<Meal> getMeals();
+
+  // Löscht ein meal
   void deleteMeal(Meal meal);
+
+  // Updated ein Meal (oldMeal) mit einem neuen (newMeal)
   void updateMeal(Meal oldMeal, Meal newMeal);
 }
--- data/mock_database.dart
+++ data/mock_database.dart
@@ -1,23 +1,32 @@
 import '../models/meal.dart';
 import 'database_repository.dart';
 
+// Unsere MockDatabase Implementierung. Dadurch dass sie unser abstraktes DatabaseRepository implementiert, müssen wir hier
+// alle Methoden aus unserem DatabaseRepository implementieren.
+// Wir verwenden erstmal eine einfache private Liste (_meals), um unsere "Datenbank" zu mocken.
+
 class MockDatabase implements DatabaseRepository {
   final List<Meal> _meals = [];
+
+  // fügt unserer Liste ein Meal hinzu
   @override
   void addMeal(Meal meal) {
     _meals.add(meal);
   }
 
+// Löscht ein Meal aus unserer Liste
   @override
   void deleteMeal(Meal meal) {
     _meals.remove(meal);
   }
 
+// gibt uns unsere Liste _meals zurück
   @override
   List<Meal> getMeals() {
     return _meals;
   }
 
+// ändert ein Meal
   @override
   void updateMeal(Meal oldMeal, Meal newMeal) {
     if (_meals.contains(oldMeal)) {
